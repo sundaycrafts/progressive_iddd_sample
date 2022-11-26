7e2ef23a0fd368098f773cf88b43db2f9d93cbad
Hiroto Murai <mh@indivi.org>
2022-11-26 11:40:05 +0000

    add specs for calendar app service

---
+++ iddd_collaboration/src/test/java/com/example/iddd_collaboration/collaboration/application/calender/CalenderApplicationServiceTest.java
@@ -0,0 +1,26 @@
+package com.example.iddd_collaboration.collaboration.application.calender;
+
+import org.junit.jupiter.api.Test;
+
+public class CalenderApplicationServiceTest {
+
+    @Test
+    void changeCalendarDescription() {
+    }
+
+    @Test
+    void createCalendar() {
+    }
+
+    @Test
+    void renameCalendar() {}
+
+    @Test
+    void scheduleCalendarEntry() {}
+
+    @Test
+    void shareCalendarWith() {}
+
+    @Test
+    void unshareCalendarWith() {}
+}
