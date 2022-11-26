a4c0b430e9322b67b0c7e010046162b5c2eea1ab
sundaycrafts <sundaycrafts@users.noreply.github.com>
2022-11-26 01:43:57 +0100

    add iddd_collaboration module with spring boot

---
+++ iddd_collaboration/src/main/java/com/example/iddd_collaboration/IdddCollaborationApplication.java
@@ -0,0 +1,13 @@
+package com.example.iddd_collaboration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdddCollaborationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdddCollaborationApplication.class, args);
+	}
+
+}
